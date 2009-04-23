96e6a94cf9689175047771373926e57a35dec176
Jim Gay <jim@saturnflyer.com>
2009-04-22 21:16:02 -0400

    more tags

--- lib/portfolio_tags.rb
+++ lib/portfolio_tags.rb
@@ -1,12 +1,14 @@
 module PortfolioTags
   include Radiant::Taggable
+
+  class TagError < StandardError; end
   
   [:clients, :projects].each do |portfolio_model|
     tag "#{portfolio_model.to_s}" do |tag|
       tag.expand
     end
     tag "#{portfolio_model.to_s}:each" do |tag|
-      collection = portfolio_model.to_s.classify.constantize.find(:all)
+      collection = portfolio_model.to_s.classify.constantize.find(:all, collection_find_options(tag))
       tag.locals.send("#{portfolio_model}=", collection)
       result = ''
       collection.each do |item|
@@ -15,6 +17,27 @@ module PortfolioTags
       end
       result
     end
+    tag "#{portfolio_model.to_s}:each:name" do |tag|
+      tag.locals.send("#{portfolio_model.to_s.singularize}").send(:name)
+    end
+  end
+  
+  def collection_find_options(tag)
+    attr = tag.attr.symbolize_keys
+    
+    options = {}
+
+    [:limit, :offset].each do |symbol|
+      if number = attr[symbol]
+        if number =~ /^\d{1,4}$/
+          options[symbol] = number.to_i
+        else
+          raise TagError.new("`#{symbol}' attribute of `each' tag must be a positive number between 1 and 4 digits")
+        end
+      end
+    end
+    
+    options
   end
   
 end
--- spec/models/portfolio_tags_spec.rb
+++ spec/models/portfolio_tags_spec.rb
@@ -11,6 +11,25 @@ describe Page do
       it "should expand it's contents for each client" do
         pages(:home).should render('<r:clients:each>text </r:clients:each>').as('text text text text text ')
       end
+      describe "with limit" do
+        it "should limit the records" do
+          pages(:home).should render('<r:clients:each limit="3">text </r:clients:each>').as('text text text ')
+        end
+        it "should error with a non-numeric limit" do
+          pages(:home).should render('<r:clients:each limit="abc">text </r:clients:each>').with_error("`limit' attribute of `each' tag must be a positive number between 1 and 4 digits")
+        end
+        it "should offset the records" do
+          pages(:home).should render('<r:clients:each limit="2" offset="1"><r:name /> </r:clients:each>').as('Ignite Social Media Kratos Defense ')
+        end
+        it "should error with a non-numeric offset" do
+          pages(:home).should render('<r:clients:each limit="3" offset="abc">text </r:clients:each>').with_error("`offset' attribute of `each' tag must be a positive number between 1 and 4 digits")
+        end
+      end
+      describe "name" do
+        it "should display the client name" do
+          pages(:home).should render('<r:clients:each><r:name /> </r:clients:each>').as('Saturn Flyer Ignite Social Media Kratos Defense Smithsonian Bright Yellow Jacket ')
+        end
+      end
     end
   end
   
@@ -21,6 +40,25 @@ describe Page do
     describe "each" do
       it "should expand it's contents for each project" do
         pages(:home).should render('<r:projects:each>text </r:projects:each>').as('text text text text ')
+      end
+      describe "with limit" do
+        it "should limit the records" do
+          pages(:home).should render('<r:projects:each limit="3">text </r:projects:each>').as('text text text ')
+        end
+        it "should error with a non-numeric limit" do
+          pages(:home).should render('<r:projects:each limit="abc">text </r:projects:each>').with_error("`limit' attribute of `each' tag must be a positive number between 1 and 4 digits")
+        end
+        it "should offset the records" do
+          pages(:home).should render('<r:projects:each limit="2" offset="1"><r:name /> </r:projects:each>').as('iris invertebrates ')
+        end
+        it "should error with a non-numeric offset" do
+          pages(:home).should render('<r:projects:each limit="3" offset="abc">text </r:projects:each>').with_error("`offset' attribute of `each' tag must be a positive number between 1 and 4 digits")
+        end
+      end
+      describe "name" do
+        it "should display the client name" do
+          pages(:home).should render('<r:projects:each><r:name /> </r:projects:each>').as('ignite website iris invertebrates byj site ')
+        end
       end
     end
   end
